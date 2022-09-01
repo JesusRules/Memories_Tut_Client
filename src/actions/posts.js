9b8c4ef95124f089db7c51476a28b1a4f8bb24c4
JesusRules <36013969+JesusRules@users.noreply.github.com>
2022-09-01 05:44:29 -0400

    Fixed problem - 4:40 hours in

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -13,14 +13,17 @@ export const getPosts = () => async (dispatch) => {
 }
 
 export const createPost = (post) => async (dispatch) => {
-    try{
-        const { data } = await api.createPost(post);
-
-        dispatch({ type: CREATE, payload: data });
-    } catch(error) {
-        console.log(error);
+    try {
+    const { data } = await api.createPost(post);
+    // const { data } = await api.fetchPosts();
+  
+      dispatch({ type: CREATE, payload: data });
+    } catch (error) {
+        
+      console.log(error.message);
     }
-}
+  };
+
 
 export const updatePost = (id, post) => async (dispatch) => {
     try {   
